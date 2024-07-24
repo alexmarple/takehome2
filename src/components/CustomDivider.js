102722cc91febfa6a563327e5b2d0e926ce06e7f
Alex Marple <54377734+alexmarple@users.noreply.github.com>
2024-07-24 13:35:28 -0700

    Change defaultProps to default parameters

--- src/components/CustomDivider.js
+++ src/components/CustomDivider.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Divider, Box } from '@mui/material';
 
-export default function CustomDivider({ pt, pb }) {
+export default function CustomDivider({ pt = 0, pb = 0 }) {
   return (
     <Box sx={{ width: '100%', pt, pb }}>
       <Divider sx={{ borderColor: '#D9D9D9' }} />
@@ -13,9 +13,4 @@ export default function CustomDivider({ pt, pb }) {
 CustomDivider.propTypes = {
   pt: PropTypes.number,
   pb: PropTypes.number,
-};
-
-CustomDivider.defaultProps = {
-  pt: 0,
-  pb: 0,
 };
