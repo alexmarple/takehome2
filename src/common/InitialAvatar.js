212598238f420988c7f55330ca10764e5c2142a0
Alex Marple <54377734+alexmarple@users.noreply.github.com>
2024-07-26 10:55:56 -0700

    Move InitialAvatar logic to a common folder for use everywhere

---
+++ src/common/InitialAvatar.js
@@ -0,0 +1,12 @@
+export default function InitialAvatar(name) {
+  const initials = name
+    ? `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`
+    : '';
+  return {
+    sx: {
+      backgroundColor: '#D9D9D9',
+      color: '#484848',
+    },
+    children: initials,
+  };
+}
