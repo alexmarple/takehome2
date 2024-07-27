96d114228cadae1ebc24458a9f3cfcca0d1580a1
Alex Marple <54377734+alexmarple@users.noreply.github.com>
2024-07-27 12:31:01 -0700

    Patient API hook

--- src/hooks/usePatients.js
+++ src/hooks/usePatients.js
@@ -2,9 +2,16 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchPatients } from '../actions';
 
+const selectPatients = (state) => state.patients;
+const selectLoading = (state) => state.loading;
+const selectError = (state) => state.error;
+
 const usePatients = () => {
   const dispatch = useDispatch();
-  const { loading, patients, error } = useSelector((state) => state);
+
+  const loading = useSelector(selectLoading);
+  const patients = useSelector(selectPatients);
+  const error = useSelector(selectError);
 
   useEffect(() => {
     dispatch(fetchPatients());
